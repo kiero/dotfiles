815549a5966e2170117a818a6e04887bc25d3705
Konrad Kierys <konrad.kierys@gmail.com>
2017-04-02 17:36:21 +0200

    Make install script

--- ruby/install.sh
+++ ruby/install.sh
@@ -5,10 +5,6 @@
 set -e
 
 install_rubies () {
-  echo "Installing Ruby 1.9.3-p551"
-  RUBY_CONFIGURE_OPTS=--with-readline-dir="$(brew --prefix readline)" rbenv install 1.9.3-p551
-  echo "Installing Ruby 2.0.0-p648"
-  RUBY_CONFIGURE_OPTS=--with-readline-dir="$(brew --prefix readline)" rbenv install 2.0.0-p648
   echo "Installing Ruby 2.2.6"
   RUBY_CONFIGURE_OPTS=--with-readline-dir="$(brew --prefix readline)" rbenv install 2.2.6
   echo "Installing Ruby 2.3.3"
@@ -17,7 +13,7 @@ install_rubies () {
   RUBY_CONFIGURE_OPTS=--with-readline-dir="$(brew --prefix readline)" rbenv install 2.4.1
 }
 
-if (( $+commands[rbenv] ))
+if test $(which rbenv)
 then
   install_rubies
 fi
