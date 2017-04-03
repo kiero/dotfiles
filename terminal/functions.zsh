096a9ae318a374146e6441ac78c8282b6baef534
Konrad Kierys <konrad.kierys@gmail.com>
2017-04-03 19:22:22 +0200

    Add function `cls` that shows numerical chmods

---
+++ terminal/functions.zsh
@@ -0,0 +1,6 @@
+# Set of functions for terminal usage
+
+# Shows numerical chmod permissions at the beginning of line
+function cls() {
+  ls -l | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/)*2^(8-i));if(k)printf("%0o ",k);print}'
+}
