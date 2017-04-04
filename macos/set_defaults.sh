84719c2e59e1457473f938d1f111a09adf34b164
Konrad Kierys <konrad.kierys@gmail.com>
2017-04-04 23:36:17 +0200

    Remove unwanted settings in macOS defaults

--- macos/set_defaults.sh
+++ macos/set_defaults.sh
@@ -105,9 +105,6 @@ defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 # Finder: show status bar
 defaults write com.apple.finder ShowStatusBar -bool true
 
-# Finder: show path bar
-defaults write com.apple.finder ShowPathbar -bool true
-
 # Display full POSIX path as Finder window title
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
