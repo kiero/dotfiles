83fb70c2b6540fd71d97a71c2febd97c045c461b
Konrad Kierys <konrad.kierys@gmail.com>
2017-04-04 23:48:46 +0200

    Add alias to remove macOS "Open With" duplicates

--- macos/aliases.zsh
+++ macos/aliases.zsh
@@ -1,3 +1,6 @@
 # Show and hide hidden files in Finder
 alias show='defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder'
 alias hide='defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder'
+
+# Clean up LaunchServices to remove duplicates in the "Open With" menu
+alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
