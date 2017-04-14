dcb684a22eaa392ce43c15bc246363d53f6aa78a
Konrad Kierys <konrad.kierys@gmail.com>
2017-04-14 23:58:01 +0200

    Add install script to vim topic

---
+++ vim/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# This installs Vim stuff
+
+# Install Vundle (vim plugin manager)
+echo "Installing Vim dependencies."
+
+VUNDLE_DIRECTORY="$HOME/.vim/bundle/Vundle.vim"
+if [ ! -d "$VUNDLE_DIRECTORY" ]; then
+  echo "Installing Vundle."
+  git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE_DIRECTORY
+fi
+vim +PluginInstall +qall
+echo "Vim dependencies have been successfully installed."
