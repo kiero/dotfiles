fe2b1cb9d0a43a039500a5754cf9992a8738ba0a
Konrad Kierys <konrad.kierys@gmail.com>
2017-04-19 08:20:03 +0200

    Set screenshots name in macOS defaults

--- macos/set_defaults.sh
+++ macos/set_defaults.sh
@@ -97,6 +97,9 @@ defaults write com.apple.screensaver askForPasswordDelay -int 0
 # Save screenshots to the desktop
 defaults write com.apple.screencapture location -string "${HOME}/Desktop"
 
+# Set screenshots name
+defaults write com.apple.screencapture name "Screenshot"
+
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
 
