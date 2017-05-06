7de4d4db3f1d0f6dc814f6e132eeb07e35307407
Konrad Kierys <konrad.kierys@gmail.com>
2017-05-06 22:37:12 +0200

    Add aliases for Homebrew

---
+++ homebrew/aliases.zsh
@@ -0,0 +1,8 @@
+# Homebrew shortcuts
+alias brif="brew info"
+alias brin="brew install"
+alias brsr="brew search"
+
+alias bcif="brew cask info"
+alias bcin="brew cask install"
+alias bcsr='brew cask search'
