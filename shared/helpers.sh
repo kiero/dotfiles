873498300f814d19c0f8165f7a621cbb18c516c2
Konrad Kierys <konrad.kierys@gmail.com>
2017-07-05 18:49:55 +0200

    Update `ask_user_default_yes()` shared function
    Now read function automatically returns after reading 1 character.

--- shared/helpers.sh
+++ shared/helpers.sh
@@ -4,7 +4,10 @@
 ask_user_default_yes() {
   local request=$1
   local result=
-  read -r -p "$request [n/Y] " response
+  read -r -n 1 -p "$request [n/Y] " response
+  # empty echo in order to display new message in a new line
+  # after reading from user input
+  echo
   case $response in
     [yY][eE][sS]|[yY]|"")
         return 0;
