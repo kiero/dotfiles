83f3471babb8c203d56b33f42e91a0be46e9a800
Konrad Kierys <konrad.kierys@gmail.com>
2021-07-17 17:49:14 +0200

    Remove RPROMPT from shell

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -31,6 +31,9 @@ PROMPT_COLOR_RESET="%{$reset_color%}"
 PROMPT_GIT_INFO='$(git_prompt_info)'
 PROMPT_SIGN="%{$fg[white]%}→%{$reset_color%}"
 
+# Right prompt, e.g. ruby-2.7.4p191
+PROMPT_RUBY_VERSION="%{$fg[magenta]%}(ruby-$(ruby --version | cut -f2 -d ' '))%{$reset_color%}"
+
 PROMPT="
 ${PROMPT_BEGINNING} \
 ${PROMPT_USER} \
@@ -39,10 +42,6 @@ ${PROMPT_HOST} \
 ${PROMPT_DIR_SEPARATOR} \
 ${PROMPT_DIR} \
 ${PROMPT_COLOR_RESET}\
-${PROMPT_GIT_INFO}
+${PROMPT_GIT_INFO} \
+${PROMPT_RUBY_VERSION}
 ${PROMPT_SIGN} "
-
-# Right prompt, e.g. ruby-2.3.0p0
-PROMPT_RUBY_VERSION="%{$fg[magenta]%}ruby-$(ruby --version | cut -f2 -d ' ')%{$reset_color%}"
-
-RPROMPT="${PROMPT_RUBY_VERSION}"
