f0cbcdf001496664f59e5028ee14c7c61323d587
Konrad Kierys <konrad.kierys@gmail.com>
2026-02-16 00:15:06 +0100

    Clean Brewfiles. Add homebrew alias

--- homebrew/aliases.zsh
+++ homebrew/aliases.zsh
@@ -2,7 +2,9 @@
 alias brif="brew info"
 alias brin="brew install"
 alias brsr="brew search"
+alias brun="brew uninstall"
 
 alias bcif="brew info --cask"
 alias bcin="brew install --cask"
+alias bcun="brew uninstall --cask"
 alias bcsr='brew search'
