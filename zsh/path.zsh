048fd047fce9cc4756f0ad9f05dd898b28731381
Konrad Kierys <konrad.kierys@gmail.com>
2026-02-16 00:10:54 +0100

    Add paths to openjdk and windsurf

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -9,4 +9,6 @@ PATH=$PATH:/usr/local/sbin
 PATH=$PATH:/usr/sbin
 PATH=$PATH:/sbin
 PATH=$PATH:/usr/local/texlive/2021basic/bin/universal-darwin
+PATH=$PATH:/opt/homebrew/opt/openjdk@11/bin
+PATH=$PATH:/Users/kiero/.codeium/windsurf/bin
 export PATH
