1b8b0082aeca146073a612d95b465d3d129872c5
Konrad Kierys <konrad.kierys@gmail.com>
2026-03-21 15:18:55 +0100

    Add aliases to set status bar in iOS simulator

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -1,4 +1,9 @@
 # iOS and macOS development aliases
+
+# Set simulator status bar to screenshot-ready state (9:41, full bars)
+alias simstatusbar="xcrun simctl status_bar booted override --time 9:41 --cellularBars 4 --cellularMode active --wifiBars 3"
+# Reset simulator status bar to default state
+alias simstatusbarreset="xcrun simctl status_bar booted clear"
 alias pi="bundle exec pod install"
 alias pu="bundle exec pod update"
 alias pru="bundle exec pod repo update"
